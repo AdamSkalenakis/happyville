5f0e738c830df6ffd9690ae6c0d40151d351fbd2
Jiyambi <sarah.ann.herzog@gmail.com>
2012-05-08 15:30:47 -0700

    Adds documentation comments

--- happyville/happyville/Game.cs
+++ happyville/happyville/Game.cs
@@ -1,3 +1,13 @@
+/************************************************************************************
+ * Game.cs          Contains main game loops and logic
+ * Project		    happyville - a social game involving people and monsters and 
+ *                  their interactions.
+ * Author		    Sarah Herzog 
+ * Version		    0.1
+ * All content in this file is copyright Sarah Herzog and Adam Skalenakis, 2012, 
+ * all rights reserved.
+ ************************************************************************************/
+#region Libraries
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,45 +18,57 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+#endregion
 
 namespace happyville
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game : Microsoft.Xna.Framework.Game
     {
+        #region Data Members
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        #endregion
 
+        #region Initialization
+
+        /****************************************************************************
+         * Constructor  Creates a new game
+         * Arguments    ---
+         * Returns      ---
+         ****************************************************************************/
         public Game()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
-            // Change of resolution.
+            // Change of resolution
             graphics.PreferredBackBufferWidth = 1152;
             graphics.PreferredBackBufferHeight = 864;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        /****************************************************************************
+         * Initialize() Allows the game to perform any initialization it needs to 
+         *              before starting to run. This is where it can query for any 
+         *              required services and load any non-graphic related content.  
+         *              Calling base.Initialize will enumerate through any components
+         *              and initialize them as well.
+         * Arguments    ---
+         * Returns      ---
+         ****************************************************************************/
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // Turn mouse visibility on
             this.IsMouseVisible = true;
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        /****************************************************************************
+         * LoadContent()    LoadContent will be called once per game and is the place 
+         *                  to load all of your content.
+         * Arguments        ---
+         * Returns          ---
+         ****************************************************************************/
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -55,20 +77,25 @@ namespace happyville
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
+        /****************************************************************************
+         * UnloadContent()  UnloadContent will be called once per game and is the 
+         *                  place to unload all content.
+         * Arguments        ---
+         * Returns          ---
+         ****************************************************************************/
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
+        #endregion
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        #region Update
+        /****************************************************************************
+         * Update()     Allows the game to run logic such as updating the world,
+         *              checking for collisions, gathering input, and playing audio.
+         * Arguments    GameTime gameTime - Provides a snapshot of timing values
+         * Returns      ---
+         ****************************************************************************/
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
@@ -79,11 +106,14 @@ namespace happyville
 
             base.Update(gameTime);
         }
+        #endregion
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        #region Draw
+        /****************************************************************************
+         * Draw()       This is called when the game should draw itself.
+         * Arguments    GameTime gameTime - Provides a snapshot of timing values
+         * Returns      ---
+         ****************************************************************************/
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.White);
@@ -92,5 +122,6 @@ namespace happyville
 
             base.Draw(gameTime);
         }
+        #endregion
     }
 }
