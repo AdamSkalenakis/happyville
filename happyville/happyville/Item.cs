57e065477cee6f11613049dbea56f0a3c1bd07ba
Jiyambi <sarah.ann.herzog@gmail.com>
2012-05-08 15:58:13 -0700

    Adds Item, Entity, and Player to class heirarchy

---
+++ happyville/happyville/Item.cs
@@ -0,0 +1,22 @@
+﻿/************************************************************************************
+ * Item.cs          Base class from which all in-game objects are derived
+ * Project		    happyville - a social game involving people and monsters and 
+ *                  their interactions.
+ * Author		    Sarah Herzog 
+ * Version		    0.1
+ * All content in this file is copyright Sarah Herzog and Adam Skalenakis, 2012, 
+ * all rights reserved.
+ ************************************************************************************/
+#region Libraries
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion
+
+namespace happyville
+{
+    class Item
+    {
+    }
+}
---
+++ happyville/happyville/Player.cs
@@ -0,0 +1,22 @@
+﻿/************************************************************************************
+ * Player.cs        An Entity controlled by a user.
+ * Project		    happyville - a social game involving people and monsters and 
+ *                  their interactions.
+ * Author		    Sarah Herzog 
+ * Version		    0.1
+ * All content in this file is copyright Sarah Herzog and Adam Skalenakis, 2012, 
+ * all rights reserved.
+ ************************************************************************************/
+#region Libraries
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion
+
+namespace happyville
+{
+    class Player : Entity
+    {
+    }
+}
