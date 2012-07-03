8300bfa0d60d8b5565df095504cd9c776b9159db
Adam Skalenakis <almightynu@gmail.com>
2012-07-03 10:20:18 -0700

    Beefball test project for movement style 2 (LMB RMB)

--- Beefball/Beefball/Beefball/Entities/PlayerBall.cs
+++ Beefball/Beefball/Beefball/Entities/PlayerBall.cs
@@ -25,6 +25,9 @@ namespace Beefball.Entities
 {
     public partial class PlayerBall
     {
+        float desiredX = 0;
+        float desiredY = 0;
+        
         private void CustomInitialize()
         {
 
@@ -33,8 +36,40 @@ namespace Beefball.Entities
 
         private void CustomActivity()
         {
-            float movementSpeed = 30;
-            InputManager.Xbox360GamePads[0].ControlPositionedObject(this, movementSpeed);
+            float movementSpeed = 5;
+
+            // Keyboard control
+            // InputManager.Xbox360GamePads[0].ControlPositionedObject(this, movementSpeed);
+
+
+            // Mouse Control
+            if (GuiManager.Cursor.PrimaryDown)
+            {
+                desiredX = GuiManager.Cursor.WorldXAt(this.Z);
+                desiredY = GuiManager.Cursor.WorldYAt(this.Z);
+                float xChange = desiredX - this.X;
+                float yChange = desiredY - this.Y;
+                double temp1 = xChange;
+                double temp2 = yChange;
+                temp1 = Math.Pow(Math.Pow(temp1, 2.0) + Math.Pow(temp2, 2.0), 0.5);
+                float temp3 = Convert.ToSingle(temp1);
+
+                float XPush = xChange * movementSpeed / temp3;
+                float YPush = yChange * movementSpeed / temp3;
+
+                this.XVelocity = XPush;
+                this.YVelocity = YPush;
+            }
+            if (desiredX == this.X || desiredY == this.Y)
+            {
+                this.XVelocity = 0;
+                this.YVelocity = 0;
+
+            }
+
+
+
+
             float desiredRotation = 361;
             float rotateSpeed = 0.05F;
             if (Math.Abs(this.XVelocity) > 0 || Math.Abs(this.YVelocity) > 0)
@@ -43,7 +78,7 @@ namespace Beefball.Entities
                 this.YVelocity, this.XVelocity);
 
             }
-            if (GuiManager.Cursor.PrimaryDown)
+            if (GuiManager.Cursor.SecondaryDown)
             {
 
                 float worldX = GuiManager.Cursor.WorldXAt(this.Z);
